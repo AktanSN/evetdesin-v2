319444b9da12ebfb9a3f3b2ab05bd63d575b97ae
Aktan Sanhal <aktansanhal@gmail.com>
2023-12-17 02:20:04 +0300

    update

---
+++ evetdesinv3/js/main.js
@@ -0,0 +1,147 @@
+
+
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+    
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+    $(document).ready(function () {
+        // Mevcut ready bloğunuzun içeriği...
+
+        // WhatsApp mesaj fonksiyonu
+        function sendWhatsapp() {
+            var phonenumber = "+905444550690";
+            var name = $('.name').val();
+            var surname = $('.surname').val();
+            var email = $('.email').val();
+            var phone = $('.phone').val();
+            var message = $('.message').val();
+
+            var url = "https://wa.me/" + phonenumber + "?text=" +
+                      "İsim: " + encodeURIComponent(name) + "%0a" +
+                      "Soyisim: " + encodeURIComponent(surname) + "%0a" +
+                      "Email: " + encodeURIComponent(email) + "%0a" +
+                      "Telefon Numarası: " + encodeURIComponent(phone) + "%0a" +
+                      "Mesaj: " + encodeURIComponent(message) + "%0a%0a" +
+                        "Bu mesaj evetdesin.com üzerinden gönderilmiştir.";
+                      
+
+            window.open(url, '_blank').focus();
+        }
+
+        // Butona tıklandığında WhatsApp mesaj fonksiyonunu çalıştır
+        $('.your-whatsapp-button-class').click(sendWhatsapp);
+    });
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+    
+})(jQuery);
+
